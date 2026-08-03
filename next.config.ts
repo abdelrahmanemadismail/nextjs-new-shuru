7fbb84358823a73524d01c72c9c01721a52e7756
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2026-08-03 16:29:11 +0300

    chore: add @napi-rs/canvas to serverExternalPackages in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['@napi-rs/canvas'],
   compress: true,
   poweredByHeader: false,
   images: {
